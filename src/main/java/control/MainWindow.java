621f9d91cfd6421abdb1fd657097197e81967f9f
rogerroque <reroqueo01@elpuig.xeill.net>
2021-03-25 16:55:05 +0100

    ffs

--- src/main/java/control/MainWindow.java
+++ src/main/java/control/MainWindow.java
@@ -1,6 +1,5 @@
 package control;
 
-import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
@@ -24,30 +23,15 @@ import java.util.*;
 public class MainWindow implements Initializable {
     private Scene scene;
     private GraphicsContext gc;
-    private Globo globo = new Globo(new Image("images/globo.png", 100, 100, false, false));
-    private Globo globo1 = new Globo(new Image("images/globo.png", 100, 100, false, false));
     private Image fons;
-    double cuantos = 0.006;
+    double cuantos = 0.005;
     private int contadorGlobosReventados;
-    Font font = Font.font("Arial");
-    Random random = new Random();
+    private Random random = new Random();
     private int numRandom;
-    private double FPS = 0.005;
+    private double FPS = 0.006;
+    private int nivel = 1;
 
     ArrayList<Globo> globos = new ArrayList<>();
-
-    /**
-     * Opció 1: Animationtimer
-     * Controlar la velocitat de moviment no és tant fàcil
-     */
-    private AnimationTimer animationTimer = new AnimationTimer() {
-        @Override
-        public void handle(long l) {
-            /*globo.clear(gc);*/
-            globo.move();
-            globo.render(gc);
-        }
-    };
 
     /**
      * Opció 2: TimeLine
@@ -59,7 +43,7 @@ public class MainWindow implements Initializable {
         @Override
         public void handle(ActionEvent event) {
 
-            numRandom = random.nextInt(3) + 1;
+            numRandom = random.nextInt(4) + 1;
             if (Math.random() < cuantos)  {
 
                 if (numRandom == 1) {
@@ -68,8 +52,11 @@ public class MainWindow implements Initializable {
                 } else if (numRandom == 2){
                     Globo globo = new Globo(new Image("images/globo2.png", 100, 100, false, false));
                     globos.add(globo);
+                } else if (numRandom == 3){
+                    Globo globo = new Globo(new Image("images/globo2.png", 100, 100, false, false));
+                    globos.add(globo);
                 } else {
-                    Globo globo = new Globo(new Image("images/globo3.png", 100, 100, false, false));
+                    Globo globo = new Globo(new Image("images/globo4.png", 100, 100, false, false));
                     globos.add(globo);
                 }
 
@@ -83,18 +70,33 @@ public class MainWindow implements Initializable {
 
             gc.setFill(Color.BLUE);
             gc.setFont(new Font("Arial", 50));
-            gc.fillText(String.valueOf(contadorGlobosReventados), 1100, 100);
+            gc.fillText(String.valueOf(contadorGlobosReventados), 1120, 80);
 
-            if (contadorGlobosReventados == 5) {
-                FPS = 0.025;
-            } else if (contadorGlobosReventados == 10) {
-                FPS = 0.035;
-            } else if (contadorGlobosReventados == 15) {
-                FPS = 0.045;
+            gc.setFill(Color.BLUE);
+            gc.setFont(new Font("Arial", 50));
+            gc.fillText("Nivel: " + nivel, 60, 80);
+
+            if (contadorGlobosReventados == 10) {
+                cuantos = 0.008;
+                nivel = 2;
             } else if (contadorGlobosReventados == 20) {
-                FPS = 0.055;
-            } else if (contadorGlobosReventados == 25) {
-                FPS = 0.065;
+                cuantos = 0.0010;
+                nivel = 3;
+            } else if (contadorGlobosReventados == 30) {
+                cuantos = 0.012;
+                nivel = 4;
+            } else if (contadorGlobosReventados == 40) {
+                cuantos = 0.014;
+                nivel = 5;
+            } else if (contadorGlobosReventados == 50) {
+                cuantos = 0.016;
+                nivel = 6;
+            } else if (contadorGlobosReventados == 60) {
+                cuantos = 0.016;
+                nivel = 7;
+            } else if (contadorGlobosReventados == 70) {
+                cuantos = 0.016;
+                nivel = 8;
             }
 
         }
@@ -128,22 +130,20 @@ public class MainWindow implements Initializable {
         scene = sc;
         scene.setOnMouseClicked(mouseEvent -> {
             Point2D point = new Point2D(mouseEvent.getX(),mouseEvent.getY());
-            globos.removeIf(globo -> {
 
-                if(globo.isClicked(point)) {
+
+            for (int i = 0; i < globos.size() ; i++) {
+                if(globos.get(i).isClicked(point)) {
                     contadorGlobosReventados++;
+
+                    // Cambiar imagen del globo por una explsion
+                    globos.get(i).setImage(new Image("images/explosion.png", 100, 100, false, false));
+
                 }
-
-                return false;
-            });
-        });
-
-
-
-        scene.setOnKeyPressed(keyEvent -> {
-            System.out.println(keyEvent.getCode().toString());
-            globo.setDirection(keyEvent.getCode().toString());
-
+            }
+            globos.removeIf(globo -> globo.isClicked(point));
         });
     }
+
+
 }
